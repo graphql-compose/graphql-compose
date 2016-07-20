510a0b140594a113b43de37d015267811660b744
nodkz <nodkz@mail.ru>
2016-07-20 12:55:26 +0600

    Export more internal useful functions

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ export { default as TypeComposer } from './typeComposer';
 export { default as InputTypeComposer } from './inputTypeComposer';
 export { default as ComposeStorage } from './storage';
 export { getProjectionFromAST } from './projection';
+export { default as GQC } from './gqc';
 
 export {
   Resolver,
@@ -24,3 +25,6 @@ export {
   toInputObjectType,
   convertInputObjectFieldOpts,
 } from './toInputObjectType';
+
+export * from './utils/misc';
+export * from './utils/is';
