ee461903282938b337961000f12ec7cc19a9fe0a
Rungsikor Rungsikavarnich <rungsikorn@me.com>
2017-10-09 13:56:25 +0700

    docs(example): add example case, use with graphql-tools (#96)
    * docs(example): add example case, use with graphql-tools
    
    * docs(example) remove self import and use graphql-compose from npm registry

---
+++ examples/with-graphql-tools/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const schema = require('./schema')
+const bodyParser = require('body-parser')
+const { graphiqlExpress, graphqlExpress } = require('apollo-server-express')
+const server = express()
+server.use(bodyParser.json())
+server.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
+server.use('/graphql', graphqlExpress((req) => ({
+  schema
+})))
+
+server.listen(3001, function () { console.log('app launch on 3000') })
