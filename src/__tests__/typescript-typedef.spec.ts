a95ebf0d5a70d55bb825877a3a55c11431255b04
nodkz <pavel.chertorogov@gmail.com>
2018-08-30 02:44:23 +0600

    brings new errors and notes

--- src/__tests__/typescript-typedef.spec.ts
+++ src/__tests__/typescript-typedef.spec.ts
@@ -53,12 +53,27 @@ const PersonTC = TypeComposer.create<Person, Context>({
 // ****************************
 
 // TC.getResolver()
-PersonTC.getResolver<any>('findOne').wrapResolve(next => rp => {
+// TODO: should be without errors! By default in resolvers source should be `any` even if provided TypeComposer.create<Person, Context> 
+PersonTC.getResolver('findOne').wrapResolve(next => rp => {
   rp.source.name = 5; // source any
   rp.source.age = 'string';
 
   return next(rp);
 });
+
+// BUT I see your what you want to do. So let provide source correctly to `wrapResolve` function:
+// We keep standard resolvers untyped forsource. BUT almost all wrappers created for some exact type,
+// so let make it typed! I think it's a right place.
+interface QuerySource { name: number, age: number };
+schemaComposer.Query.addFields({
+  user: PersonTC.getResolver('findOne').wrapResolve<QuerySource>(next => rp => {
+    rp.source.name = 5; // source any
+    rp.source.age = 'string'; // <----- here must be an error
+  
+    return next(rp);
+  })
+});
+
 
 // TResolverSource is Person or you may specify
 PersonTC.getResolver('findOne').wrapResolve(next => rp => {
@@ -67,7 +82,7 @@ PersonTC.getResolver('findOne').wrapResolve(next => rp => {
 
   // source Person | undefined  as a result of Partial<ResolveParams...
   if (rp.source) {
-    rp.source.name = 'string';
+    rp.source.name = 2;  // <---- for backward compatibility for TS users, here should be mo errors (by default source: any)
     rp.source.age = 5;
   }
 
@@ -76,12 +91,12 @@ PersonTC.getResolver('findOne').wrapResolve(next => rp => {
 
 // TC.addResolver()
 // add resolver with no type passed in
-PersonTC.addResolver<any>({
+PersonTC.addResolver({   // <---------------- by default resolver has `source: any` an it can not be defined explicitly like it was
   resolve: ({ source, context }) => {
     // check availability
     if (source && context) {
       source.age = 2;
-      source.name = 'XYZ';
+      source.name = 2;           // <---- should not be error
       if (context.uid !== source.uid) {
         // do something if not the current user ...
       }
@@ -90,7 +105,7 @@ PersonTC.addResolver<any>({
 });
 
 // add resolver with a source type
-PersonTC.addResolver<Person>({
+PersonTC.addResolver<Person>({ // <--- this is ok, if we want to provide really Basic/Global/Generic interface, which will be implemented in several types/models. And then to this types we will add this resolver.
   resolve: ({ source, context }) => {
     // check availability
     if (source && context) {
@@ -128,9 +143,9 @@ PersonTC.wrapResolverResolve<Person>('findMany', next => rp => {
 // Fields
 // No Overloads created for fields
 // ****************************
-PersonTC.getFieldTC<DeepOptions>('deep')
+PersonTC.getFieldTC('deep') // <----- no matter what type you provide here, it should not affect on rp.source below
   .getResolver('findOne')
-  .wrapResolve(next => rp => {
+  .wrapResolve<DeepOptions>(next => rp => { // <----- provide type be here, if you want to check rp.source
     // check source and context because they are defined as Partial<ResolveParams...
     if (rp.source && rp.context) {
       rp.source.deep1 = 'string';
@@ -138,7 +153,7 @@ PersonTC.getFieldTC<DeepOptions>('deep')
     }
   });
 
-PersonTC.getFieldTC<any>('deep')
+PersonTC.getFieldTC('deep') // <-------------- this case should not have errors
   .getResolver('findOne')
   .wrapResolve(next => rp => {
     // no checking of rp.source as it is any, but we check context as it is inherited
@@ -154,29 +169,44 @@ PersonTC.getFieldTC<any>('deep')
     }
   });
 
-// addFields
-interface PersonExtended extends Person {
-  new1: string;
-  new2: number;
-  extends: string; // Person
-}
 
-// If you want to use the extended person source types then you need a variable to collect
-const PersonExtendedTC = PersonTC.addFields<PersonExtended>({
-  new1: 'String',
-  new2: 'Int'
-});
+// -------- miss understanding, corrext tests provided below
 
-PersonExtendedTC.getResolver('findOne').wrapResolve(next => rp => {
-  if (rp.source && rp.context) {
-    rp.source.new1 = 'string';
-    rp.context.uid = 'string'; // passes
-  }
-});
+// // addFields
+// interface PersonExtended extends Person {
+//   new1: string;
+//   new2: number;
+//   extends: string; // Person
+// }
 
-PersonExtendedTC.getResolver<any>('findOne').wrapResolve(next => rp => {
-  rp.source.new1 = 5;
-});
+// // If you want to use the extended person source types then you need a variable to collect
+// const PersonExtendedTC = PersonTC.addFields<PersonExtended>({
+//   new1: 'String',
+//   new2: 'Int'
+// });
+
+// PersonExtendedTC.getResolver('findOne').wrapResolve(next => rp => {
+//   if (rp.source && rp.context) {
+//     rp.source.new1 = 'string';
+//     rp.context.uid = 'string'; // passes
+//   }
+// });
+
+// PersonExtendedTC.getResolver<any>('findOne').wrapResolve(next => rp => {
+//   rp.source.new1 = 5;
+// });
+
+// adding new field to graphql type, its fieldConfig.resolve method should have TSource type
+PersonTC.addFields({
+  ageIn2030: {
+    type: 'Int',
+    resolve: (source) => {  // <------------  here `source` MUST have `Person` type
+      return source.age + 12;
+    },
+  },
+})
+
+
 
 // **************************
 // Relations
@@ -184,18 +214,22 @@ PersonExtendedTC.getResolver<any>('findOne').wrapResolve(next => rp => {
 // This particular case has been a bit of a problem, that is before i did the changes
 // resolver was always conflicting with the invoking TC
 // **************************
-PersonExtendedTC.addRelation<Person>('extends', {
+interface Art { id: number, personId: string } 
+const ArtTC = schemaComposer.getOrCreateTC<Art>('Art');
+
+ArtTC.addRelation('extends', { // <------------ NO NEED IN addRelation<Person> here 
   resolver: PersonTC.getResolver('findById'), // comes from other (resolve to)
   prepareArgs: {
-    _id: source => source.extends  // type checks well now
+    _id: source => source.personId  // type checks well now
   },
-  projection: { extends: true }
+  projection: { personId: true }
 });
 
-PersonExtendedTC.addRelation('extends', {
-  resolver: PersonTC.getResolver('findById'), // comes from other (resolve to)
-  prepareArgs: {
-    _id: source => source.extends  // type checks well now
-  },
-  projection: { extends: true }
-});
+// <----------- became the same as above, so comment this case.
+// ArtTC.addRelation('extends', {
+//   resolver: PersonTC.getResolver('findById'), // comes from other (resolve to)
+//   prepareArgs: {
+//     _id: source => source.extends  // type checks well now
+//   },
+//   projection: { extends: true }
+// });
