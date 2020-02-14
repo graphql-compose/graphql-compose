d5d9b60c71a9c17a099f5e182ce619dedb998bae
Morgan Touverey Quilling <mtouverey@methodinthemadness.eu>
2020-02-14 17:42:24 +0100

    test: rename badly-named variable

--- src/__tests__/github_issues/231-test.js
+++ src/__tests__/github_issues/231-test.js
@@ -4,18 +4,18 @@ import { schemaComposer } from '../..';
 
 describe('github issue #231: Cloning a resolver evaluates its configuration thunks', () => {
   it('clone a resolver without evaluating its type and args thunks', async () => {
-    const anArgThunk = jest.fn(() => 'String');
+    const aTypeThunk = jest.fn(() => 'String');
 
     const aResolver = schemaComposer.createResolver({
-      name: anArgThunk,
+      name: aTypeThunk,
       type: 'Boolean',
       args: {
-        anArg: anArgThunk,
+        anArg: aTypeThunk,
       },
     });
 
     aResolver.clone();
 
-    expect(anArgThunk).not.toBeCalled();
+    expect(aTypeThunk).not.toBeCalled();
   });
 });
