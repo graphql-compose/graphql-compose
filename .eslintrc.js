fb01eaabb0d084b6bb7b5ef0a714bed2d7dd3bb2
nodkz <pavel.chertorogov@gmail.com>
2021-04-27 00:20:56 +0600

    refactor: remove babel, now project is transpiled by typescript itself
    BREAKING CHANGE: remove `mjs`, `ejs` builds

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,7 @@ const path = require('path');
 module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'prettier'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier', 'plugin:prettier/recommended'],
   parserOptions: {
     sourceType: 'module',
     useJSXTextNode: true,
